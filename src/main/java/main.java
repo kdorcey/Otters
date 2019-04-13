8b8f135ca7ef8a2a25de54b648f00443b7e00840
Kyle <kdorcey5103@gmail.com>
2019-04-13 18:12:06 -0500

    cleaned up main.java for now until it's needed

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,27 +1,7 @@
-
+import java.io.IOException;
 
 public class main {
-    public static void main(String args[]) throws IOException{
-        File studentInfo = new File("Student_Data/Student Info.xlsx");
-        FileInputStream fis = new FileInputStream(studentInfo);
-        XSSFWorkbook workbook = new XSSFWorkbook(fis);
-        XSSFSheet sheet = workbook.getSheetAt(0);
+    public static void main(String args[]){
 
-        Iterator<Row> rowIterator = sheet.iterator();
-
-        while(rowIterator.hasNext()){
-            Row row = rowIterator.next();
-
-            Iterator<Cell> cellIterator = row.cellIterator();
-
-            while(cellIterator.hasNext()){
-                Cell cell = cellIterator.next();
-                System.out.println(cell.toString());
-            }
-            System.out.println();
-        }
-
-        workbook.close();
-        fis.close();
     }
 }

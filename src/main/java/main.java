cdf7776080d4d1328d5317052678853e99b78e0e
Kyle <35466861+kdorcey@users.noreply.github.com>
2019-04-14 16:07:34 -0500

    temporary test that the collectAllStudentData() and calculateAverageGrade() methods work

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -2,6 +2,14 @@ import java.io.IOException;
 
 public class main {
     public static void main(String args[]){
+        XLSXReader dataCollector = new XLSXReader("Student_Data/Student Info.xlsx",
+                "Student_Data/Test Scores.xlsx", "Student_Data/Test Retake Scores.xlsx" );
+
+        dataCollector.collectAllStudentData();
+
+        System.out.println(dataCollector.calculateAverageGrade());
+        //80+85+80+93+92+75+99+100+98
+
 
     }
 }

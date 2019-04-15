d5c7e20e0ea59b2b9dac8666fa94814aa316bbfc
Kyle <kdorcey5103@gmail.com>
2019-04-15 13:15:35 -0500

    added very basic driver function

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,13 +1,21 @@
 import java.io.IOException;
+import java.util.HashMap;
 
 public class main {
     public static void main(String args[]){
         XLSXReader dataCollector = new XLSXReader("Student_Data/Student Info.xlsx",
                 "Student_Data/Test Scores.xlsx", "Student_Data/Test Retake Scores.xlsx" );
 
-        dataCollector.collectAllStudentData();
+        HashMap allStudents = dataCollector.collectAllStudentData();
+        System.out.println(Student.calculateAverageGrade());
+        for(Student temp:Student.fCompSciStu){
+            System.out.println(temp);
+        }
 
-        System.out.println(dataCollector.calculateAverageGrade());
+
+        //dataCollector.collectAllStudentData();
+
+        //System.out.println(Student.calculateAverageGrade());
         //80+85+80+93+92+75+99+100+98
 
 

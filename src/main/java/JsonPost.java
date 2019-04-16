69898e7aae055dfd75d70143259f9a9a360538a4
Kyle <kdorcey5103@gmail.com>
2019-04-16 12:09:27 -0500

    added dependency for creating JSON objects. Also created class for the JSON post.

---
+++ src/main/java/JsonPost.java
@@ -0,0 +1,15 @@
+import java.util.Set;
+import org
+
+public class JsonPost {
+
+    private Set<Student> fCompSciStudents;
+    private int gradeAverage;
+
+    public JsonPost(Set<Student> fCompSciStudents, int gradeAverage){
+        this.fCompSciStudents = fCompSciStudents;
+        this.gradeAverage = gradeAverage;
+
+
+    }
+}

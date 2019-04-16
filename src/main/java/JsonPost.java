24677e89675fd33eb422854be78d0c0f70f9851c
Kyle <kdorcey5103@gmail.com>
2019-04-16 16:29:34 -0500

    logic for making HTTP POST with the JSON and driver function

--- src/main/java/JsonPost.java
+++ src/main/java/JsonPost.java
@@ -1,15 +1,50 @@
-import java.util.Set;
-import org
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.*;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.json.*;
 
 public class JsonPost {
 
-    private Set<Student> fCompSciStudents;
-    private int gradeAverage;
+    private JSONObject jsonToPost;
 
-    public JsonPost(Set<Student> fCompSciStudents, int gradeAverage){
-        this.fCompSciStudents = fCompSciStudents;
-        this.gradeAverage = gradeAverage;
+    public JsonPost(HashMap<Integer, Student> fCompSciStudents, int gradeAverage){
+        ArrayList<Integer> test = new ArrayList<Integer>(fCompSciStudents.keySet());
+        Collections.sort(test);
 
+        jsonToPost = new JSONObject();
+        jsonToPost.put("id","dorceykyle@gmail.com");
+        jsonToPost.put("name", "Kyle Dorcey");
+        jsonToPost.put("average",gradeAverage);
+        jsonToPost.put("studentIds", test);
 
     }
+
+    public void post() throws Exception{
+        URL url = new URL("http://3.86.140.38:5000/challenge");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setConnectTimeout(8000);
+        connection.setReadTimeout(8000);
+
+        connection.setRequestMethod("POST");
+        connection.setDoOutput(true);
+        connection.setRequestProperty("Content-Type", "application/json");
+
+        OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream());
+        out.write(jsonToPost.toString());
+        out.flush();
+        out.close();
+
+        System.out.println(connection.getResponseCode());
+
+        connection.disconnect();
+    }
+
 }
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,22 +1,14 @@
 import java.util.HashMap;
 
 public class main {
-    public static void main(String args[]){
+    public static void main(String args[]) throws Exception{
         DataCollector dataCollector = new DataCollector("Student_Data/Student Info.xlsx",
                 "Student_Data/Test Scores.xlsx", "Student_Data/Test Retake Scores.xlsx" );
 
         HashMap allStudents = dataCollector.collectAllStudentData();
-        System.out.println(dataCollector.calculateAverageGrade());
-        for(Student temp:dataCollector.getfCompSciStu()){
-            System.out.println(temp);
-        }
 
-
-        //dataCollector.collectAllStudentData();
-
-        //System.out.println(Student.calculateAverageGrade());
-        //80+85+80+93+92+75+99+100+98
-
+        JsonPost test = new JsonPost(dataCollector.getfCompSciStu(), dataCollector.calculateAverageGrade());
+        test.post();
 
     }
 }
